392f6d322f53b2bd1074a455442fb235dcda763d
liujiahuan <liujiahuan@elensdata.com>
2021-03-04 12:46:46 +0800

    svg size

--- src/lib/components/icons/loading.vue
+++ src/lib/components/icons/loading.vue
@@ -1,14 +1,14 @@
 <!-- from https://codepen.io/aurer/pen/jEGbA -->
 <template>
   <div class="loader" title="loading…">
-    <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="20px" height="20px" viewBox="0 0 20 20" style="enable-background:new 0 0 50 50;" xml:space="preserve">
-        <rect x="0" y="0" width="4" height="20" :fill="color">
+    <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="16px" height="16px" viewBox="0 0 16 16" style="enable-background:new 0 0 50 50;" xml:space="preserve">
+        <rect x="0" y="0" width="3" height="16" :fill="color">
             <animate attributeName="opacity" attributeType="XML" values="1; .2; 1" begin="0s" dur="0.6s" repeatCount="indefinite" />
         </rect>
-        <rect x="7" y="0" width="4" height="20" :fill="color">
+        <rect x="7" y="0" width="3" height="16" :fill="color">
             <animate attributeName="opacity" attributeType="XML" values="1; .2; 1" begin="0.2s" dur="0.6s" repeatCount="indefinite" />
         </rect>
-        <rect x="14" y="0" width="4" height="20" :fill="color">
+        <rect x="14" y="0" width="3" height="16" :fill="color">
             <animate attributeName="opacity" attributeType="XML" values="1; .2; 1" begin="0.4s" dur="0.6s" repeatCount="indefinite" />
         </rect>
     </svg>
